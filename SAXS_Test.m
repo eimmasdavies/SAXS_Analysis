afb2fcc298259ef9fdef77cf4ddddd8fbe6b9171
eimmasdavies <94634409+eimmasdavies@users.noreply.github.com>
2021-11-18 17:44:32 +0000

    Add files via upload

---
+++ SAXS_Test.m
@@ -0,0 +1,11 @@
+pip install h5py
+pip install numpy
+pip install matplotlib
+
+import h5py
+
+
+import numpy as np 
+
+data == np.loadtxt('i22-569965_saxs_0,0.dat', skiprows == 0, delimiter == '\t')
+data.shape
